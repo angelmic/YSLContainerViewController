d154a02d5f1a3c068934d1844f4a8293c529bff7
richard <lichang.liu@gmail.com>
2017-10-02 17:32:21 +0800

    [Update] change style and morden objc

--- YSLContainerViewController/YSLScrollMenuView.h
+++ YSLContainerViewController/YSLScrollMenuView.h
@@ -17,15 +17,15 @@
 @interface YSLScrollMenuView : UIView
 
 @property (nonatomic, weak) id <YSLScrollMenuViewDelegate> delegate;
-@property (nonatomic, strong) UIScrollView *scrollView;
-@property (nonatomic, strong) NSArray *itemTitleArray;
-@property (nonatomic, strong) NSArray *itemViewArray;
+@property (nonatomic, strong) UIScrollView                 *scrollView;
+@property (nonatomic, strong) NSArray                      *itemTitleArray;
+@property (nonatomic, strong) NSArray                      *itemViewArray;
 
-@property (nonatomic, strong) UIColor *viewbackgroudColor;
-@property (nonatomic, strong) UIFont *itemfont;
-@property (nonatomic, strong) UIColor *itemTitleColor;
-@property (nonatomic, strong) UIColor *itemSelectedTitleColor;
-@property (nonatomic, strong) UIColor *itemIndicatorColor;
+@property (nonatomic, strong) UIColor                      *viewbackgroudColor;
+@property (nonatomic, strong) UIFont                       *itemfont;
+@property (nonatomic, strong) UIColor                      *itemTitleColor;
+@property (nonatomic, strong) UIColor                      *itemSelectedTitleColor;
+@property (nonatomic, strong) UIColor                      *itemIndicatorColor;
 
 - (void)setShadowView;
 
